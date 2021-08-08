2fae4506f2d5b5febe2897ba15a0234aabff328c
jaredl85 <jaredloveridge@gmail.com>
2021-08-08 09:31:31 -0700

    Added router back in to project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import Home from './components/Home.js';
-import NeChiro from './components/NeChiro.js';
-import Portfolio from './components/Portfolio.js';
-import DogWithin from './components/DogWithin.js';
-import NuCamp from './components/NuCamp.js';
-import { Switch, Route } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
-import './App.css';
+import React from "react";
+import Home from "./components/Home.js";
+import NeChiro from "./components/NeChiro.js";
+import Portfolio from "./components/Portfolio.js";
+import DogWithin from "./components/DogWithin.js";
+import NuCamp from "./components/NuCamp.js";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <ScrollToTop>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/nechiro" exact component={NeChiro} />
+          <Route path="/dogwithin" exact component={DogWithin} />
+          <Route path="/nucamp" exact component={NuCamp} />
+          <Route path="/portfolio" exact component={Portfolio} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+    </ScrollToTop>
   );
 }
 
